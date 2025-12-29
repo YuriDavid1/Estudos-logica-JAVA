2367da2e6d43a616caa8feff3a168be0558ab60b
YuriDavid1 <Yuri111713k@gmail.com>
2025-12-28 23:05:13 -0300

    Conclusão da Conta Bancaria

--- 04 - ArrayList-ForEach/Conta-Bancaria-Refatorada/ContaBancaria.java
+++ 04 - ArrayList-ForEach/Conta-Bancaria-Refatorada/ContaBancaria.java
@@ -1,32 +1,54 @@
-package Poo2;
-
-
-public class ContaBancaria {
+	package Poo2;
+	import java.util.Scanner;
 	
-	private String titular;
-	private double saldo;
+	public class ContaBancaria {
 	
-	public ContaBancaria(String titular) {
-		this.titular = titular;
-		this.saldo = 0.0;
-	}
-	
-	public void depositar(double valor) {
-		saldo += valor;
-		System.out.println("Depósito de R$" + valor + " realizado para " + titular);
-	}
-	
-	public void sacar(double valor) {		
-		if(valor <= saldo) {
-			saldo -=valor;
-			System.out.println("Saque de R$" + valor + " Realizado comm Sucesso."); 
+		Scanner ler = new Scanner (System.in);
+		private String titular;
+		private double saldo;
+		
+		public ContaBancaria(String titular) {
+			this.titular = titular;
+			this.saldo = 0.0;
+		}
+		
+		public void depositar(double deposito) {
+		
+		if(deposito > 0) {
+			saldo += deposito;	
+			System.out.println("R$"+deposito + " foram depositados a sua conta.");
+			System.out.println("Saldo atual: R$" +saldo);
 		}else {
-			System.out.println("Saldo insuficiente para saque.");
+			System.out.println("Insira um valor valido.");
+			}
+		}
+		
+		public void sacar(double sacar) {
+			if(saldo >= sacar) {
+				saldo -=sacar;
+				System.out.println("R$" + sacar + " foram retirados de sua conta.");
+				System.out.println("Saldo atual: R$" + saldo);
+			}else {
+				System.out.println("Este valor ultrapassou o seu saldo.");
+			}
+		}
+		
+		
+		public String consultar() {
+			return titular +  "   |    " + saldo;
+		}
+		
+		//GETS
+		public String getTitular() {
+			return titular;
+		}
+		
+		public double getSaldo() {
+			return saldo;
+		}
+		
+		//SETS
+		public void setSaldo(double saldo) {
+			this.saldo = saldo;
 		}
 	}
-	
-	public void consultarSaldo() {
-	System.out.println("Titular: " + titular + " | " + "Saldo: " + saldo);
-	}
-	
-}
--- 04 - ArrayList-ForEach/Conta-Bancaria-Refatorada/Main.java
+++ 04 - ArrayList-ForEach/Conta-Bancaria-Refatorada/Main.java
@@ -5,51 +5,82 @@ import java.util.ArrayList;
 public class Main {
 
 	public static void main(String[]args) {
+		
+		ArrayList<ContaBancaria> contas = new ArrayList<>();
 		Scanner ler = new Scanner(System.in);
 		int menu = 0;
 		
-		ArrayList<ContaBancaria> contas = new ArrayList<>();
-		
-		while(menu !=5) {
-			System.out.println("----------MENU----------");
-			System.out.println("[1] Cadastrar Conta");
-			System.out.println("[2] Listar Contas");
+		do {
+			System.out.println("==========  MENU  ==========");
+			System.out.println("[1] Cadastrar conta");
+			System.out.println("[2] Listar contas");
 			System.out.println("[3] Depositar");
 			System.out.println("[4] Sacar");
 			System.out.println("[5] Sair");
-			System.out.println("Insira uma das opções acima.");
+			System.out.println("Insira uma opção valida: ");
 			menu = ler.nextInt();
 			ler.nextLine();
 			
-		switch(menu) {
-		case 1:
-			System.out.println("Insira o Nome do Titular: ");
-			String titu = ler.nextLine();
-			ContaBancaria novaConta = new ContaBancaria(titu);
-			contas.add(novaConta);
-			int indice = contas.size();
-			System.out.println("Numero da Conta: " +  indice);
-			
-		/*case 2:
-			double valorsaque;
-			System.out.println("Insira o valor que deseja sacar.");
-			valorsaque = ler.nextDouble();
-			ler.nextLine();
-			cliente.sacar(valorsaque);
-			break;
-			
-		case 3:
-		cliente.consultarSaldo();		
-		break;
-		
-		case 4: 
-			System.out.println("Encerrando...");
-			break;
-			
+			switch(menu) {
+			case 1:
+				System.out.println("Insira o nome do titular: ");
+				String titular = ler.nextLine();
+				ContaBancaria pessoa = new ContaBancaria(titular);
+				contas.add(pessoa);
+				int id = contas.size() -1;
+				System.out.println("Numero da conta: " + id);
+				
+				break;
+				
+			case 2:
+				System.out.println("========== LISTA DE CONTAS ==========");
+				System.out.println("ID  |   " + "|   Titular   " + "|   Saldo   ");
+				for(int i = 0; i<contas.size(); i++) {
+					System.out.println(i + "  |   " +  contas.get(i).consultar());
+				}
+				
+				break;
+				
+			case 3:
+				System.out.println("Insira o numero da conta (ID)");
+				id = ler.nextInt();
+				ler.nextLine();
+				if(id >= 0 && id < contas.size()) {
+					System.out.println("Insira o valor que deseja depositar:");
+					double deposito = ler.nextDouble();
+					ler.nextLine();
+					contas.get(id).depositar(deposito);					
+					
+				}else {
+					System.out.println("Insira um ID de conta válido.");
+				}				
+				break;
+				
+			case 4:
+				System.out.println("Insira o numero da da conta (ID)");
+				id = ler.nextInt();
+				ler.nextLine();
+				if(id >=0 && id < contas.size()) {
+					System.out.println("Insira o valor que deseja sacar: ");
+					double saque = ler.nextDouble();
+					ler.nextLine();
+					contas.get(id).sacar(saque);
+				}else {
+					System.out.println("Insira um ID de conta válido.");
+				}
+				
+				break;
+				
+			case 5:
+				System.out.println("Saindo. Até mais...");
+				break;
+				
 			default:
-				System.out.println("Insira um número valido ao menu.");
-				*/
-			}	
-		}
+				System.out.println("Opção incorreta. Insira uma opção valida.");
+			}
+			
+		}while(menu !=5);
+		
 	}
+	
 }
