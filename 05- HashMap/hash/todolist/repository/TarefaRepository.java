e0715010209c89f77df5d0e4b11274b4dfc2d86b
YuriDavid1 <Yuri111713k@gmail.com>
2026-01-04 23:31:01 -0300

    Atualização da todoList

--- 05- HashMap/hash/todolist/repository/TarefaRepository.java
+++ 05- HashMap/hash/todolist/repository/TarefaRepository.java
@@ -4,9 +4,28 @@ import java.util.HashMap;
 import todolist.model.*;
 
 public class TarefaRepository {
+	
+	HashMap<Integer, Tarefa> tarefaHash = new HashMap<>();
+	
+	
+	//Função salvar Tarefa
+	public void salvarTarefa(Tarefa tarefa) {
+		tarefaHash.put(tarefa.getID(), tarefa);
+	}
+	
+	//Função buscar tarefa por ID
+	public Tarefa buscarporID (int id) {
+		return tarefaHash.get(id);
+	}
 
+	// Função remover por ID
+	public void removerporId(int id) {
+		tarefaHash.remove(id);
+	}
 	
-	HashMap<Integer, Tarefa> tarefasHash = new HashMap<>();
-	
-	
+	//Função listar Todas tarefa
+	public HashMap<Integer, Tarefa>  listarTodos(){
+		return tarefaHash;
+	}
+    
 }
