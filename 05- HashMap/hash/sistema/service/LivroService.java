40bdd8f14894a4b33985c9cd21752843c8abd676
YuriDavid1 <Yuri111713k@gmail.com>
2026-01-10 21:10:54 -0300

    Criação do service, e inicio da view.

--- 05- HashMap/hash/sistema/service/LivroService.java
+++ 05- HashMap/hash/sistema/service/LivroService.java
@@ -1,11 +1,15 @@
 package sistema.service;
 import sistema.model.Livro;
+import java.util.HashMap;
+
 import sistema.repository.LivroRepository;
 
 public class LivroService {
-
-	private Livro livro;
-	private LivroRepository livroRepository;
+	private LivroRepository repositorio;
+	
+	public LivroService(LivroRepository repositorio) {
+		this.repositorio = repositorio;
+	}
 	
 	public boolean cadastrarLivro(Livro livro) {
 		if(livro.getId() <= 0) {
@@ -13,30 +17,75 @@ public class LivroService {
 			return false;
 		}
 		
+		if(repositorio.buscarLivro(livro.getId()) != null){
+			System.out.println("ID de livro já cadastrado.");
+			return false;
+		}
 		
+		if(livro.getAutor() == null || livro.getAutor().isEmpty()) {
+			System.out.println("É obrigatório ter um autor.");
+		return false;
+		}
 		
-		
-		
+		if(livro.getTitulo() == null || livro.getTitulo().isEmpty()) {
+			System.out.println("É obrigatório ter um titulo.");
+		return false;
+		}
+		System.out.println("Livro cadastrado.");
+		repositorio.salvarLivro(livro);
 		return true;
 	}
 	
-	public void removerLivroV() {
+	public boolean removerLivro(int id) {
+		if(repositorio.buscarLivro(id) == null) {
+			System.out.println("Livro não encontrado para este ID.");
+			return false;
+		}
 		
+		System.out.println("Livro removido com êxito.");
+		repositorio.removerLivro(id);
+		return true;
 	}
 	
-	public void buscarLivroV() {
-		
+	public Livro buscarLivro(int id) {
+		return repositorio.buscarLivro(id);
+	}
+
+	
+	public boolean emprestarLivro(int id) {
+		Livro livro = repositorio.buscarLivro(id);
+		if(livro == null) {
+			System.out.println("Livro não encontrado para este ID.");
+		return false;
+		}
+		if(livro.getEmprestado()) {
+			System.out.println("Livro já emprestado.");
+			return false;
+		}
+		System.out.println("Livro emprestado.");
+		livro.setEmprestado(true);
+		return true;
 	}
 	
-	public void devolverLivro() {
-		
+	public boolean devolverLivro(int id) {
+		Livro livro = repositorio.buscarLivro(id);
+		if(livro == null) {
+			System.out.println("Livro não encontrado para este ID.");
+			return false;
+		}
+		if(!livro.getEmprestado()) {
+			System.out.println("Este livro não esta emprestado.");
+			return false;
+		}
+		System.out.println("Livro devolvido.");
+		livro.setEmprestado(false);
+		return true;
 	}
 	
-	public void emprestarLivro() {
-		
-	}
+
 	
-	public void listarLivroV() {
+	public HashMap<Integer,Livro> listarLivro() {
+		return repositorio.listarLivros();
 		
 	}
 	
--- 05- HashMap/hash/sistema/view/Menu.java
+++ 05- HashMap/hash/sistema/view/Menu.java
@@ -1,5 +1,11 @@
 package sistema.view;
+import sistema.model.*;
+import sistema.repository.*;
+import sistema.service.*;
 
 public class Menu {
 
+	
+	
+	
 }
