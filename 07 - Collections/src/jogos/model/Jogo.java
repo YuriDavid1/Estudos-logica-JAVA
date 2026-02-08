208689bc6cf18366116e7020a0730ba0741f6e93
Yuri <Yuri111713k@gmail.com>
2026-02-08 17:43:21 -0300

    Model: Jogo e Usuario

--- 07 - Collections/src/jogos/model/Jogo.java
+++ 07 - Collections/src/jogos/model/Jogo.java
@@ -2,4 +2,39 @@ package jogos.model;
 
 public class Jogo {
 
-}
+	int id;
+	String nome;
+	String genero;
+	boolean disponivel;
+	
+	public Jogo(int id, String nome, String genero) {
+		this.id = id;
+		this.nome = nome;
+		this.genero = genero;
+		disponivel = true;
+	}
+	
+	//Getters
+	public int getId() {
+		return id;
+	}
+	
+	public String getNome() {
+		return nome;
+	}
+	
+	public String getGenero() {
+		return genero;
+	}
+	
+	public boolean getDisponivel() {
+		return disponivel;
+	}
+	
+	//Setters
+	public void setDisponivel(boolean disponivel) {
+		this.disponivel = disponivel;
+	}
+	
+	
+	}
--- 07 - Collections/src/jogos/model/Usuario.java
+++ 07 - Collections/src/jogos/model/Usuario.java
@@ -1,5 +1,33 @@
 package jogos.model;
+import java.util.List;
+import java.util.ArrayList;
 
 public class Usuario {
 
-}
+	int id;
+	String nome;
+	List<Emprestimo> historico = new ArrayList <>();
+	
+	public Usuario (int id, String nome) {
+		this.id = id;
+		this.nome = nome;
+		this.historico = new ArrayList<>();
+	}
+	
+	   public int getId() {
+	        return id;
+	    }
+
+	    public String getNome() {
+	        return nome;
+	    }
+
+	    public List<Emprestimo> getHistorico() {
+	        return historico;
+	    }
+
+	    public void adicionarEmprestimo(Emprestimo emprestimo) {
+	        historico.add(emprestimo);
+	    }
+	}
+
